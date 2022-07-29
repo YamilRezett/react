20005de7ca193e1270667afdfabcc29a75ef3230
Your Name <yamilrezett@gmail.com>
2022-07-29 10:33:25 -0300

    29/7

---
+++ src/componets/app.js
@@ -0,0 +1,17 @@
+import Nav from "./navbar";
+
+function App(){
+    return (
+       <>
+       <Nav/>
+      <div>
+        <form action="formulario">
+
+        </form>
+      </div>
+       </>
+       
+    )
+}
+
+export default App;
