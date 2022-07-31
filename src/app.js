78f5b8337bdee1a0def38ccb7fb788a0a9f5aecf
Your Name <yamilrezett@gmail.com>
2022-07-31 17:19:02 -0300

    entrega N2

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import Nav from "./componets/nav";
+import ItemListContainer from "./componets/ItemListContainer";
+
+function App(){
+    return (
+       <>
+       <Nav/>
+      <div>
+        <ItemListContainer/>        
+      </div>
+       </>
+       
+    )
+}
+
+export default App;
---
+++ src/componets/CartWidget.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function BrandExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="Logo YR"
+              src="/android-icon-72x72.png"
+              width="56.5"
+              height="56.5"
+              className="fixed-top align-center"
+            />{' '}
+            Seguros YR
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default BrandExample;
