4bcbeb7040142a9f668970f9744886f05d1261fe
yamil <103195874+YamilRezett@users.noreply.github.com>
2022-08-16 16:34:29 -0300

    error 16/8

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Nav from "./componets/nav";
 import ItemListContainer from "./componets/ItemListContainer";
-import ItemDetailContainer from "./componets/ItemListContainer";
+// import ItemDetailContainer from "./componets/ItemListContainer";
 import { BrowserRouter } from "react-router-dom";
 
 function App(){
